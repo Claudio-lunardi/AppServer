61b6675fe7c0957d2342528c7dedd4531d5bd38a
Claudio Lunardi <90716030+Claudio-lunardi@users.noreply.github.com>
2026-02-22 18:16:58 -0300

    enviando arquivos

---
+++ Controllers/StateController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.InteropServices;
+
+namespace AppServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StateController : ControllerBase
+    {
+        private static int _contador = 0;
+
+        [HttpGet("contador")]
+        public IActionResult get()
+        {
+            _contador++;
+            return Ok(new { contador = _contador });
+        }
+    }
+}
